1ef8ceaeba5381127c5ab1f10c61c9f43ef3f61b
Bhargav Annem <bhargav.annem@gmail.com>
2024-09-06 19:02:35 -0700

    feat: parallel polynomial multiplication

--- field.py
+++ field.py
@@ -1,5 +1,9 @@
+from typing import List
 from tinygrad import Tensor, dtypes
 import numpy as np
+from scipy.linalg import toeplitz
+
+MODULUS = 2**16
 
 def binmul(v1, v2, length=None):
     if v1 < 256 and v2 < 256 and rawmulcache[v1][v2] is not None:
@@ -111,3 +115,81 @@ for i in range(256):
 
 rawmulcache = Tensor(rawmulcache)
 mulcache = Tensor(mulcache)
+
+def get_class(arg, start=int):
+    if isinstance(arg, (list, tuple)):
+        output = start
+        for a in arg:
+            output = get_class(a, output)
+        return output
+    elif start == int:
+        return arg.__class__
+    elif arg.__class__ == int:
+        return start
+    elif start == arg.__class__:
+        return arg.__class__
+    else:
+        raise Exception("Incompatible classes: {} {}".format(start, arg.__class__))
+
+def spread_type(arg, cls):
+    if isinstance(arg, cls):
+        return arg
+    elif isinstance(arg, int):
+        return cls(arg)
+    elif isinstance(arg, (list, tuple)):
+        return arg.__class__([spread_type(item, cls) for item in arg])
+    else:
+        raise Exception("Type propagation of {} hit incompatible element: {}".format(cls, arg))
+
+def enforce_type_compatibility(*args):
+    cls = get_class(args)
+    return tuple([cls] + list(spread_type(arg, cls) for arg in args))
+
+def eval_poly_at(poly, pt):
+    cls, poly, pt = enforce_type_compatibility(poly, pt)
+
+    powers = Tensor.arange(len(poly), dtype=poly.dtype)
+    powers = pt ** powers
+    
+    return (poly * powers).sum()
+    
+def add_polys(a: List[BinaryFieldElement], b: List[BinaryFieldElement]):
+    cls, a, b = enforce_type_compatibility(a, b)
+    
+    # Convert to Tensors if they're not already
+    a = Tensor([x.value for x in a]) if isinstance(a[0], BinaryFieldElement) else (Tensor(a) if not isinstance(a, Tensor) else a)
+    b = Tensor([x.value for x in b]) if isinstance(b[0], BinaryFieldElement) else (Tensor(b) if not isinstance(b, Tensor) else b)
+    
+    if len(a) == len(b):
+        return a ^ b
+
+    max_len = max(len(a), len(b))
+    a_padded = Tensor([0] * (max_len - len(a)) + a.tolist())
+    b_padded = Tensor([0] * (max_len - len(b)) + b.tolist())
+
+    return a_padded ^ b_padded
+
+def mul_polys(a: List[BinaryFieldElement], b: List[BinaryFieldElement]):
+    cls, a, b = enforce_type_compatibility(a, b)
+
+    a = [x.value for x in a] if isinstance(a[0], BinaryFieldElement) else a
+    b = [x.value for x in b] if isinstance(b[0], BinaryFieldElement) else b
+
+    # Determine the length of the result polynomial
+    result_len = len(a) + len(b) - 1
+
+    # Pad both polynomials to the result length
+    a_padded = Tensor(a + [0] * (result_len - len(a)))
+    b_padded = Tensor(b + [0] * (result_len - len(b)))
+
+    # Create the Toeplitz matrix
+    toeplitz_matrix = Tensor(toeplitz(a_padded.numpy(), np.zeros(result_len, dtype=np.uint8)))
+    result = toeplitz_matrix.dot(b_padded)
+
+    return Tensor(np.array(result.numpy())[::-1] % MODULUS)
+
+
+if __name__ == "__main__":
+    a = [1, 2, 3]
+    b = [4, 5, 6, 7, 8, 10, 12]
+    print(mul_polys(a, b).tolist())
